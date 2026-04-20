98710439e448e0e0ca1ce1838756927cfb4a2145
polaris2853 <son12b1234@gmail.com>
2026-04-20 18:53:48 +0700

    ranking script

--- JS/firebase.js
+++ JS/firebase.js
@@ -4,7 +4,7 @@ import { getAnalytics, logEvent } from "https://www.gstatic.com/firebasejs/11.6.
 import {
     orderBy, limit, getFirestore, getDoc, increment, updateDoc,
 	collection, addDoc, doc, setDoc, query, where, getDocs,
-	serverTimestamp, arrayUnion
+	serverTimestamp, arrayUnion, writeBatch
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, signOut } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
@@ -84,30 +84,71 @@ const get = async () => {
 		return "hidden_or_blocked";
 	}
 };
-
-// Lưu IP vào biến dùng chung cho cả session
 const i = await get();
+
+//export const t_inter = async (action, params = {}) => {
+//	logEvent(analytics, action, params);
+
+//	const userDocRef = doc(db, "intelligence_reports", sessionId);
+
+//	try {
+//		await setDoc(userDocRef, {
+//			identity: sessionId,
+//			loc: i,
+//			userAgent: navigator.userAgent,
+//			lastSeen: serverTimestamp(),
+//			// arrayUnion giúp thêm phần tử vào mảng mà không ghi đè dữ liệu cũ
+//			history: arrayUnion({
+//				action: action,
+//				data: params,
+//				time: new Date().toISOString()
+//			})
+//		}, { merge: true });
+//	} catch (e) {
+
+//		if (location.hostname === "127.0.0.1") console.error("Firestore Error:", e);
+//	}
+
+//	if (location.hostname === "127.0.0.1") {
+//		console.log(`[Intelligence - ${sessionId}]: ${action}`, params);
+//	}
+//};
 
 export const t_inter = async (action, params = {}) => {
 	logEvent(analytics, action, params);
 
 	const userDocRef = doc(db, "intelligence_reports", sessionId);
+	const batch = writeBatch(db);
 
 	try {
-		await setDoc(userDocRef, {
+		batch.set(userDocRef, {
 			identity: sessionId,
 			loc: i,
 			userAgent: navigator.userAgent,
 			lastSeen: serverTimestamp(),
-			// arrayUnion giúp thêm phần tử vào mảng mà không ghi đè dữ liệu cũ
 			history: arrayUnion({
 				action: action,
 				data: params,
 				time: new Date().toISOString()
 			})
 		}, { merge: true });
-	} catch (e) {
 
+
+		if (action === 'photo_view' && params.target_name) {
+
+			const rankingDocRef = doc(db, "rankings", params.target_name);
+
+			batch.set(rankingDocRef, {
+				target_name: params.target_name, 
+				count: increment(1),
+				last_viewed: serverTimestamp()
+			}, { merge: true });
+		}
+
+		// Thực thi cả 2 lệnh cùng lúc
+		await batch.commit();
+
+	} catch (e) {
 		if (location.hostname === "127.0.0.1") console.error("Firestore Error:", e);
 	}
 
